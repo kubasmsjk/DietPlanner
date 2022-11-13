372d395145120baa30cc244680931b8dad9ca4ab
krzbb <kerazeb@gmail.com>
2022-11-13 19:57:27 +0100

    validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +22,8 @@ import { UserInputFormComponent } from './user-input-form/user-input-form.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-input-form/user-input-form.component.ts
+++ src/app/user-input-form/user-input-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormsModule,ReactiveFormsModule, FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input-form',
@@ -6,11 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-input-form.component.css']
 })
 export class UserInputFormComponent implements OnInit {
-
-  constructor() { }
+  myForm = this.fb.group({
+    weight: new FormControl('', [Validators.min(20), Validators.max(200)]),
+    height: new FormControl('', [Validators.min(140), Validators.max(220)]),
+    age: new FormControl('', [Validators.min(10), Validators.max(80)])
+  })
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
   submit(){
     
   }
