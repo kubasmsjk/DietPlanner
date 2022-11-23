4e37c9d20f9625b58542299346007a955175d0cf
JakubMieczkowski(109 <jakm5000@wp.pl>
2022-11-23 18:17:01 +0100

    regrex

--- src/app/validators/name-validator.ts
+++ src/app/validators/name-validator.ts
@@ -2,6 +2,6 @@ import {FormControl} from '@angular/forms';
 
 export function nameValidator(control: FormControl){
   const surnameValue = control.value;
-  const regexp: RegExp=/^[a-z]+$/i; 
+  const regexp: RegExp=/^[a-zA-ZżźćńółęąśŻŹĆĄŚĘŁÓŃ]+$/i; 
   return regexp.test(surnameValue)? null : {onlyLetters: true};
 }
