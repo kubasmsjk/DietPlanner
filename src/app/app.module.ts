b953f7704636b3558675d259335f9fc47dcae827
JakubMieczkowski(109 <jakm5000@wp.pl>
2022-11-28 12:49:39 +0100

    db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DietTableComponent } from './body/diet-table/diet-table.component';
 import { CreateDietComponent } from './body/create-diet/create-diet.component';
 import { GrayedDirective } from './body/create-diet/grayed.directive';
 import { AdminPanelComponent } from './body/admin-panel/admin-panel.component';
+import { TransformKcalPipe } from './body/diet-table/transform-kcal.pipe';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { AdminPanelComponent } from './body/admin-panel/admin-panel.component';
     DietTableComponent,
     CreateDietComponent,
     GrayedDirective,
-    AdminPanelComponent
+    AdminPanelComponent,
+    TransformKcalPipe
   ],
   imports: [
     BrowserModule,
